4f1a2e0469f4a3b4a4d4f2a56e3e1c8ee70da9fb
murdo2458 <murdo124@gmail.com>
2024-06-16 17:08:56 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main >
-      <p className="absolute inset-0 flex justify-center items-center text-white bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-5xl font-extrabold">
+      <p className="absolute inset-0 flex justify-center items-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-5xl font-extrabold">
         Website Content Placeholder
       </p>
     </main>
