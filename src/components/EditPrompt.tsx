0b39ab9efd281afccf8ad88532a2c9ecaff5f960
murdo2458 <murdo124@gmail.com>
2024-06-20 20:49:52 +0100

    without env

--- src/components/EditPrompt.tsx
+++ src/components/EditPrompt.tsx
@@ -15,25 +15,22 @@ import { Input } from "@/components/ui/input"
 import { chatbotPrompt } from '@/app/helpers/constants/chatbot-prompt'
 import { Textarea } from "@/components/ui/textarea"
 import { neon } from '@neondatabase/serverless';
+import postgres from 'postgres';
 
-interface EditPromptProps extends HTMLAttributes<HTMLDivElement> {
 
-}
 
-const EditPrompt: FC<EditPromptProps> = ({ }) => {
+export default async function Page() {
     async function create(formData: FormData) {
         "use server";
-        const sql = neon(process.env.POSTGRES_URL);
+        const sql = neon("postgres://default:wk1ojG5JcHzR@ep-dry-pond-a4vj7rkg-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require");
         await sql`CREATE TABLE IF NOT EXISTS comments (comment TEXT)`;
         const comment = formData.get("comment");
         await sql("INSERT INTO comments (comment) VALUES ($1)", [comment]);
     }
     return (
         <form action={create}>
-            <input type="text" placeholder="write a comment" name="comment" className='text-black' />
+            <input type="text" placeholder="write a comment" name="comment" />
             <button type="submit">Submit</button>
         </form>
-    )
+    );
 }
-
-export default EditPrompt
