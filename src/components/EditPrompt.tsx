807d8ec32da31fe475ac1190bdd1700638530507
murdo2458 <murdo124@gmail.com>
2024-06-20 20:42:21 +0100

    with env variable

--- src/components/EditPrompt.tsx
+++ src/components/EditPrompt.tsx
@@ -20,10 +20,10 @@ interface EditPromptProps extends HTMLAttributes<HTMLDivElement> {
 
 }
 
-const EditPrompt: FC<EditPromptProps> = ({ className, ...props }) => {
+const EditPrompt: FC<EditPromptProps> = ({ }) => {
     async function create(formData: FormData) {
         "use server";
-        const sql = neon("postgres://default:wk1ojG5JcHzR@ep-dry-pond-a4vj7rkg-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require");
+        const sql = neon(process.env.POSTGRES_URL);
         await sql`CREATE TABLE IF NOT EXISTS comments (comment TEXT)`;
         const comment = formData.get("comment");
         await sql("INSERT INTO comments (comment) VALUES ($1)", [comment]);
