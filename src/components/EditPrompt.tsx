b4953ad29ab6da6e8154d83dcd44bd4552389afa
murdo2458 <murdo124@gmail.com>
2024-06-20 20:39:21 +0100

    postgres post without shad input

--- src/components/EditPrompt.tsx
+++ src/components/EditPrompt.tsx
@@ -29,38 +29,10 @@ const EditPrompt: FC<EditPromptProps> = ({ className, ...props }) => {
         await sql("INSERT INTO comments (comment) VALUES ($1)", [comment]);
     }
     return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant='default'>
-                    View Prompt</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Edit Prompt</DialogTitle>
-                    <DialogDescription>
-                        Make changes to the AI prompt
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    {/* <div className="grid grid-cols-4 grid-rows-50 items-center gap-4"> */}
-                    {/* <Label htmlFor="name" className="text-left">
-                            Prompt
-                        </Label> */}
-                    {/* <Textarea
-                            className="text-black row-span-10 col-span-10"
-                            value={chatbotPrompt} /> */}
-                    {/* </div> */}
-                </div>
-                <DialogFooter>
-                    <form action={create}>
-                        <input type="text" placeholder="write a comment" name="comment" className='text-black' />
-                        <button type="submit">Submit</button>
-                    </form>
-                    {/* <Button variant='outline'>
-                        Save changes</Button> */}
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
+        <form action={create}>
+            <input type="text" placeholder="write a comment" name="comment" className='text-black' />
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
