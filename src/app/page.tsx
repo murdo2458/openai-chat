874e70c5407a8e48b8f4fe96de3041191b0106e5
murdo2458 <murdo124@gmail.com>
2024-06-20 16:25:38 +0100

    components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main >
       <p className="inset-0 flex justify-center items-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 text-5xl font-extrabold">
-        MD's support Chatbot. Powered by OpenAI
+        MDs support Chatbot. Powered by OpenAI
       </p>
     </main>
   );
