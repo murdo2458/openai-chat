0353cb6fa229dceb31d18ad34851b329665c2d0d
murdo2458 <murdo124@gmail.com>
2024-06-20 20:29:18 +0100

    postgres input

--- src/components/EditPrompt.tsx
+++ src/components/EditPrompt.tsx
@@ -20,14 +20,14 @@ interface EditPromptProps extends HTMLAttributes<HTMLDivElement> {
 
 }
 
-const EditPrompt: FC<EditPromptProps> = ({ }) => {
-    // async function create(formData: FormData) {
-    //     "use server";
-    //     const sql = neon($process.env.POSTGRES_URL);
-    //     await sql`CREATE TABLE IF NOT EXISTS comments (comment TEXT)`;
-    //     const comment = formData.get("comment");
-    //     await sql("INSERT INTO comments (comment) VALUES ($1)", [comment]);
-    // }
+const EditPrompt: FC<EditPromptProps> = ({ className, ...props }) => {
+    async function create(formData: FormData) {
+        "use server";
+        const sql = neon("postgres://default:wk1ojG5JcHzR@ep-dry-pond-a4vj7rkg-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require");
+        await sql`CREATE TABLE IF NOT EXISTS comments (comment TEXT)`;
+        const comment = formData.get("comment");
+        await sql("INSERT INTO comments (comment) VALUES ($1)", [comment]);
+    }
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -42,19 +42,22 @@ const EditPrompt: FC<EditPromptProps> = ({ }) => {
                     </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 grid-rows-50 items-center gap-4">
-                        <Label htmlFor="name" className="text-left">
+                    {/* <div className="grid grid-cols-4 grid-rows-50 items-center gap-4"> */}
+                    {/* <Label htmlFor="name" className="text-left">
                             Prompt
-                        </Label>
-                        <Textarea
+                        </Label> */}
+                    {/* <Textarea
                             className="text-black row-span-10 col-span-10"
-                            value={chatbotPrompt} />
-                    </div>
+                            value={chatbotPrompt} /> */}
+                    {/* </div> */}
                 </div>
                 <DialogFooter>
-                    <Button variant='outline'
-                    >
-                        Save changes</Button>
+                    <form action={create}>
+                        <input type="text" placeholder="write a comment" name="comment" className='text-black' />
+                        <button type="submit">Submit</button>
+                    </form>
+                    {/* <Button variant='outline'>
+                        Save changes</Button> */}
                 </DialogFooter>
             </DialogContent>
         </Dialog>
