503102012a25edbe6fa3c14a6a53942b68a394cf
murdo2458 <murdo124@gmail.com>
2024-10-21 15:39:58 +0100

    remove error handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,20 @@ import type { NextRequest } from 'next/server'
 import { rateLimiter } from '@/lib/rate-limiter'
 
 
-export async function middleware(req: NextRequest) {
-    const ip = req.ip ?? 'localhost'
+// export async function middleware(req: NextRequest) {
+//     const ip = req.ip ?? 'localhost'
 
-    try {
-        const { success } = await rateLimiter.limit(ip)
+//     try {
+//         const { success } = await rateLimiter.limit(ip)
 
-        if (!success) return new NextResponse('Slow down there bud, this API aint free.')
-        return NextResponse.next()
-    } catch (error) {
-        return new NextResponse(
-            'Ooops! Something went wrong.'
-        )
-    }
-}
+//         if (!success) return new NextResponse('Slow down there bud, this API aint free.')
+//         return NextResponse.next()
+//     } catch (error) {
+//         return new NextResponse(
+//             'Ooops! Something went wrong.'
+//         )
+//     }
+// }
 
 export const config = {
     matcher: '/api/message/:path*',
